2aa3d70442969e78d3bd5dd8476303e90f4fdd85
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-11 16:11:25 +0530

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,8 @@
+name=["snowball","chewy","Bubbles","Gurff"]
+animal=["cat","dog","Fish","Goat"]
+age=[1,2,2,6]
+i=0
+while i<len(name):
+    print(name[i],"the",animal[i],"is",age[i])
+    i=i+1
+  

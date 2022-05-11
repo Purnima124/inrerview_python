1c099905b378e66fb3af65dc483d7ee1f8dc8caa
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-11 16:15:54 +0530

    Add files via upload

---
+++ json.py
@@ -0,0 +1,13 @@
+# import json
+# a={"pihu":20,"purnima":30,"sakshi":60}
+# out_file = open("purnima.json", "w")
+# json.dump(a,out_file, indent = 6)
+# out_file.close() 
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+print(thisdict["model"])
